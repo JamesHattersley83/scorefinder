4881486a1f58bb14caa927ab4ad679817a9dcbfb
James Hattersley <jameshattersley83@gmail.com>
2019-03-20 22:19:28 +0000

    created teams json file

---
+++ src/components/TeamList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Team from "./Team";
+
+const TeamList = ({ teams }) => {
+  const teamArray = teams.map((team, i) => {
+    return (
+      <Team
+        id={teams[i].id}
+        crestUrl={teams[i].crestUrl}
+        name={teams[i].name}
+        founded={teams[i].founded}
+        ground={teams[i].ground}
+      />
+    );
+  });
+  return <div className="container d-flex flex-wrap">{teamArray};</div>;
+};
+
+export default TeamList;
