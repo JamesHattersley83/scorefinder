088e9d65d84d035ff02b753f47585cd71b4c00fc
James Hattersley <jameshattersley83@gmail.com>
2019-04-04 21:50:29 +0100

    Updates

---
+++ precache-manifest.8586d1e4055f50525b39a76615e727b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c59b9951d0780fd1459fbe73eaaeda7",
+    "url": "/ScoreFinder/static/media/logo.4c59b995.svg"
+  },
+  {
+    "revision": "b2d8068d692286f53947",
+    "url": "/ScoreFinder/static/js/runtime~main.b2dbbf6e.js"
+  },
+  {
+    "revision": "1c41feaa31b0b35af298",
+    "url": "/ScoreFinder/static/js/main.d0d4e1d5.chunk.js"
+  },
+  {
+    "revision": "3de7d4cdd44483c631f5",
+    "url": "/ScoreFinder/static/js/2.72d43a24.chunk.js"
+  },
+  {
+    "revision": "1c41feaa31b0b35af298",
+    "url": "/ScoreFinder/static/css/main.3fdbc363.chunk.css"
+  },
+  {
+    "revision": "781fb8bdab885d8501181ff69eccd29a",
+    "url": "/ScoreFinder/index.html"
+  }
+];
