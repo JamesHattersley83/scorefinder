771d2bd890ec42c68f1a72fcaeb2e86a90e8f5f4
James Hattersley <jameshattersley83@gmail.com>
2019-04-21 23:43:05 +0100

    added navbar links with routes and made responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,36 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark mb-5">
+    <nav className="navbar navbar-expand-sm sticky-top navbar-dark bg-dark mb-5">
       <Link to="/">
         <span className="navbar-brand mb-0 h1 mx-auto">Score Finder</span>
       </Link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul className="navbar-nav align-items-center ml-auto">
+          <li className="nav-item">
+            <Link to="/news" className="nav-link">
+              News
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/standings" className="nav-link">
+              Standings
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
