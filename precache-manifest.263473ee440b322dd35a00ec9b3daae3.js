c3fdb15f7abcfd75fdf098ce9f589f4db982126b
James Hattersley <jameshattersley83@gmail.com>
2019-04-29 22:43:12 +0100

    Updates

--- precache-manifest.263473ee440b322dd35a00ec9b3daae3.js
+++ precache-manifest.263473ee440b322dd35a00ec9b3daae3.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/ScoreFinder/static/js/runtime~main.b2dbbf6e.js"
   },
   {
-    "revision": "229a76366157ae6181d9",
-    "url": "/ScoreFinder/static/js/main.1f5466e1.chunk.js"
+    "revision": "fcc0e9eb6f425ec58946",
+    "url": "/ScoreFinder/static/js/main.92e080f3.chunk.js"
   },
   {
-    "revision": "49fcd336352c7f1f36a6",
-    "url": "/ScoreFinder/static/js/2.9d54734e.chunk.js"
+    "revision": "dc1dd97878b2908c85bf",
+    "url": "/ScoreFinder/static/js/2.120971e9.chunk.js"
   },
   {
-    "revision": "229a76366157ae6181d9",
+    "revision": "fcc0e9eb6f425ec58946",
     "url": "/ScoreFinder/static/css/main.3fdbc363.chunk.css"
   },
   {
-    "revision": "00ecd4ea7dc57afcab20cd4b80f2ec90",
+    "revision": "3f5e376d4ff5124a57bc4cc14f183a79",
     "url": "/ScoreFinder/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ScoreFinder/precache-manifest.22666f2908346738846f0720f7e81211.js"
+  "/ScoreFinder/precache-manifest.263473ee440b322dd35a00ec9b3daae3.js"
 );
 
 workbox.clientsClaim();
